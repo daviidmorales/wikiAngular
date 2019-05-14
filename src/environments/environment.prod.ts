6c478aa234942c90a250b598d930190e6b69ebf1
Brayan Riaño <briano@intergrupo.com>
2019-05-14 10:54:52 -0500

    cambios en variables puerto localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3001/',
+  apiUrl: 'http://localhost:3000/',
   userNameApi: '065399b83e51bac411059d12ec42a2d4',
   userPasswordApi: '22d1c2e80fbcec3a7aff5d3436709e94',
   encryptMethodApi: 'HS512',
